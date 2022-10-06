fb240e08f8188aff8e2607d3dbca1aea1bff6d22
linhle1512 <98157359+linhle1512@users.noreply.github.com>
2022-10-07 03:10:16 +0700

    clock first time

---
+++ Clock/Clock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClockAssign
+{
+    delegate void UpdateClockHandlers(DateTime clockTime);
+
+    class Clock
+    {
+        public event UpdateClockHandlers ClockChanged;
+
+        private DateTime clockTime;
+
+        public DateTime ClockTime
+        {
+            get => clockTime;
+
+            set
+            {
+                clockTime = value;
+                if (ClockChanged != null)
+                {
+                    ClockChanged(ClockTime);
+                }
+            }
+        }
+    }
+}
---
+++ ClockAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ClockAssign
+{
+    class Program
+    {
+        private static void GetClockTimeChanged(DateTime clockTime)
+        {
+            Console.WriteLine("New Clock: " + clockTime);
+
+            Thread.Sleep(1000);
+        }
+
+        static void Main(string[] args)
+        {
+            Clock time = new Clock();
+            time.ClockChanged += GetClockTimeChanged;
+
+            while (true)
+            {
+                time.ClockTime = DateTime.Now;
+            }
+        }
+    }   
+}
