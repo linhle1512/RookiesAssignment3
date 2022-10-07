b2c417451c29423ad37c5c640f28bb4bd98e40e4
linhle1512 <98157359+linhle1512@users.noreply.github.com>
2022-10-07 08:38:31 +0700

    remove complicated code and transfer to use basic await at prime number exercise

--- PrimeNumber/Program.cs
+++ PrimeNumber/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace PrimeNumber
@@ -9,49 +7,38 @@ namespace PrimeNumber
     {
         static void Main(string[] args)
         {
-            var primes = GetPrimeNumbers(0, 100).Result;
+            Task.WhenAny(
+            GetPrimeNumbers(0, 100),
+            GetPrimeNumbers(100, 200)
+            );
 
-            if (primes.Any())
-            {
-                foreach (var prime in primes)
-                {
-                    Console.WriteLine(prime);
-                }
-            }
-            Console.WriteLine("Total prime numbers: " + primes.Count);
+            Console.WriteLine("Task done.");
+            Console.ReadKey();
         }
 
-        static async Task<List<int>> GetPrimeNumbers(int minimum, int maximum)
+        static async Task GetPrimeNumbers(int minimum, int maximum)
         {
-            var list = new List<int>();
-
-            var result = await Task.Run(() =>
+            await Task.Run(() =>
             {
                 for (int i = minimum; i <= maximum; i++)
                 {
                     if (isPrimeNumber(i))
                     {
-                        list.Add(i);
+                        Console.WriteLine(" " + i);
                     }
                 }
-                return list;
             });
-
-            return result;
         }
-
-     
 
         public static bool isPrimeNumber(int n)
         {
-            
             if (n < 2)
             {
                 return false;
             }
-            int squareRoot = (int)Math.Sqrt(n);
+            int square = (int)Math.Sqrt(n);
             int i;
-            for (i = 2; i <= squareRoot; i++)
+            for (i = 2; i <= square; i++)
             {
                 if (n % i == 0)
                 {
